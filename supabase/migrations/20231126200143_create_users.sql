4647b5dcc986500c423b4546cd2e445034f72d45
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2023-12-06 06:07:15 +0000

    Create users table

---
+++ supabase/migrations/20231126200143_create_users.sql
@@ -0,0 +1,18 @@
+create table "public"."users" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone not null default now(),
+    "name" text not null
+);
+
+
+alter table "public"."users" enable row level security;
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."users" validate constraint "users_id_fkey";
+
+
